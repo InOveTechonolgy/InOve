d5b3dfa79d1397d693d20f9293468a244dc5d49a
InOveTechonolgy <inovetechofficial@gmail.com>
2021-09-29 09:39:55 +0530

    responsive ness added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Navbar.css"
+import logo from "../assets/imgs/logo.png"
+function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="#home" className="brand-logo">
+                <img src={logo} alt="brand-logo" />
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <div><a href="#Home" className="active-link">Home</a></div>
+                    </li>
+                    <li>
+                        <div><a href="#Servics">Services</a></div>
+                    </li>
+                    <li>
+                        <div><a href="#Technology">Technology</a></div>
+                    </li>
+                    <li>
+                        <div><a href="#About">About</a></div>
+                    </li>
+                    <li>
+                        <div><a href="#join">Join us</a></div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
